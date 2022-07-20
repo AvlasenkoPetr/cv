5d98de83753daee584f52f2e0a48cf3c31a8000f
AvlasenkoPetr <phone.petr@gmail.com>
2022-07-20 12:07:45 +0300

    editted about info

--- src/pages/MainPage/sections/AboutSection/AboutSection.tsx
+++ src/pages/MainPage/sections/AboutSection/AboutSection.tsx
@@ -6,9 +6,9 @@ export const AboutSection: React.FC = () => {
             Hi! My name is Petya, im 22 y.o Junior front-end developer from Minsk, Belarus. I've been 
             studying front-end development for a year now, during this time I've learned to quickly 
             master new technologies, find solutions to given issues, complete tasks on time and work in team. 
-            I have experience of refactoring someone else's and legacy React code, working 
-            in team repository, adding Typescript to JS \ React code and etc. Currently im looking for a full-time 
-            job, open for a relocation.
+            In addition to the basic skills of creating applications, I also have experience of refactoring someone else's 
+            and legacy React code, working in team repository, adding Typescript to JS \ React code and etc. 
+            Currently im looking for a full-time job, open for a relocation.
         </div>
     )
 }
